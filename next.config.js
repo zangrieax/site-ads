892757822f40f999f3c0b7954f60d42ea4435437
AndreyAnin <andrey.anin@ru.axept.com>
2025-08-09 02:23:59 +0300

    add

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   output: 'export',
+  distDir: 'docs',
   basePath: isProd ? '/site-ads' : '',
   assetPrefix: isProd ? '/site-ads/' : '',
   trailingSlash: true,
